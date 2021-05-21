a9cdc689b472f89b04fba4750ffb79c71a4d46f0
Steffen Vogel <post@steffenvogel.de>
2021-05-21 11:11:17 +0200

    remote FCOS support

--- fedora-coreos/butane.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-docker run --volume ${DIR}/..:/provisioning --rm quay.io/coreos/butane:release --help --files-dir=/provisioning < butane.yaml > butane.ign
